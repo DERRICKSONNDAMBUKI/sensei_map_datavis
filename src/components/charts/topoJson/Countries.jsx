b326a7afa98eb3edbf5665f38f15361321eaeb37
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-01-05 12:50:44 +0300

    topojson data feature commit

---
+++ src/components/charts/topoJson/Countries.jsx
@@ -0,0 +1,18 @@
+import { json } from "d3";
+import React, { useEffect, useState } from "react";
+import { feature } from "topojson";
+
+const jsonUrl = "https://unpkg.com/world-atlas@2.0.2/countries-50m.json";
+
+export const Countries = () => {
+  const [data, setData] = useState(null);
+  console.log(data);
+
+  useEffect(() => {
+    json(jsonUrl).then((topoJsonData) => {
+        const {countries}= topoJsonData.objects
+      setData(feature(topoJsonData,countries));
+    });
+  }, []);
+  return <h1>Contries with topojson</h1>;
+};
